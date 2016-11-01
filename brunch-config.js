819b973bdedcdabd534f1cd8ef1e8c5e448da03c
Pixelastic <tim@pixelastic.com>
2016-11-01 14:25:46 +0100

    First commit from other talk

---
+++ brunch-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  paths: {
+    watched: ['app/assets', 'app/styles/', 'app/javascripts/']
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'app.js': /^app\/javascripts\//
+      }
+    },
+    stylesheets: {joinTo: 'main.css'}
+  },
+  plugins: {
+    sass: {
+      mode: 'native'
+    },
+    babel: {
+      presets: ['es2015'],
+      ignore: [],
+      pattern: /\.js$/
+    },
+    jadePages: {
+      destination: function (path) {
+        console.info(path)
+        path.replace(/^app[\/\\](.*)\.jade$/, "$1.html")
+      },
+      jade: {
+        doctype: "html"
+      },
+      htmlmin: false
+    }
+  },
+  server: {
+    run: 'yes',
+    port: 5017
+  }
+};
+
